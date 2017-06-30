04c5f1ba30aa90490d1ffff5525d4afd5446e050
wilsonssot <wilsonss_emelec@hotmail.com>
2017-06-30 15:21:56 -0500

    Update EjemploContenedorDeCod.java
    nuevos cambios

--- ejemploContenedorDeCod/src/ejemplocontenedordecod/EjemploContenedorDeCod.java
+++ ejemploContenedorDeCod/src/ejemplocontenedordecod/EjemploContenedorDeCod.java
@@ -19,8 +19,7 @@ public class EjemploContenedorDeCod {
     public static void main(String[] args) {
         // TODO code application logic here
         JOptionPane.showMessageDialog(null, "Hola Mundo");
-        JOptionPane.showMessageDialog(null, "Modificacion desde github.com");
-        JOptionPane.showMessageDialog(null, "Tu tia");
+   
         
     }
     
