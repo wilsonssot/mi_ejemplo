c91eac4b27d49bd42c345b18538df67ae8711706
wilsonssot <wilsonss_emelec@hotmail.com>
2017-06-30 15:28:24 -0500

    Update EjemploContenedorDeCod.java
    sfdef

--- ejemploContenedorDeCod/src/ejemplocontenedordecod/EjemploContenedorDeCod.java
+++ ejemploContenedorDeCod/src/ejemplocontenedordecod/EjemploContenedorDeCod.java
@@ -19,7 +19,7 @@ public class EjemploContenedorDeCod {
     public static void main(String[] args) {
         // TODO code application logic here
         JOptionPane.showMessageDialog(null, "Hola Mundo");
-   
+                JOptionPane.showMessageDialog(null, "Hola Mundo 1");
         
     }
     
